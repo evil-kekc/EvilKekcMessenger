1296cea991b6e193f9fc93cd4b503b395e3b89c8
evil-kekc <3371824@gmail.com>
2023-07-16 19:21:04 +0300

    Refactor registration

--- account/forms.py
+++ account/forms.py
@@ -11,6 +11,10 @@ class RegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(required=True)
+    password1 = forms.CharField(label='Password',
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -19,5 +23,11 @@ class RegistrationForm(UserCreationForm):
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('Пользователь с таким email уже существует.')
+            raise forms.ValidationError('A user with this email already exists.')
         return email
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.ValidationError('Passwords don\'t match.')
+        return cd['password2']
